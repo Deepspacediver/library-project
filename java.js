7af363fc5de1d67b247ea78c19ecda6e44869063
deepspacediver <luka.koscik@gmail.com>
2022-07-04 10:05:46 +0200

    added styling and html body

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-console.log("yo")
+let library = [];
+
+function Book(author, title, pages) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+}
+
+function addBook(){
+    let userAuthor = prompt("Enter the author of the book", 'Bernard Cornwell')
+    let userTitle = prompt('Enter title of the book', 'The Last Kingdom')
+    let userPages = prompt('Enter number of pages of the book', '200')
+    const userBook = new Book(userAuthor, userTitle, userPages)
+    library.push(userBook)
+}
+
+// addBook();
