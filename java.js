39f96f997f8e6e6a0fc9890c1ff892c3fc7b3ea8
deepspacediver <luka.koscik@gmail.com>
2022-07-08 09:52:32 +0200

    divided  function adding and creating book  into two seperate

--- java.js
+++ java.js
@@ -5,81 +5,61 @@ function Book(author, title, pages) {
     this.title = title
     this.pages = pages
 }
-/* let userAuthor;
-let userTitle;
-let userPages;
-
-function createBook(){
-    userAuthor = prompt("Enter the author of the book", 'Bernard Cornwell')
-    userTitle = prompt('Enter title of the book', 'The Last Kingdom')
-    userPages = prompt('Enter number of pages of the book', '200')
-    
-} */
 
 function addBook(){
-    const userBook = new Book(userAuthor, userTitle, userPages)
+    const userBook = new Book(formAuthor.value, formTitle.value, formPages.value)
     library.push(userBook)
+    return userBook
 }
 
 const bookBody = document.querySelector('div.book-body')
 
-
-
-function createBook(){
+function createBook(bookObject){
     // library.forEach(element =>{
 
-    const userAuthor = formAuthor.value;
-    const userTitle = formTitle.value;
-    const userPages = formPages.value;
-    
-    const userBook = new Book(userAuthor, userTitle, userPages)
-    library.push(userBook)
-    
     
     const newBookDiv = document.createElement('div')
     newBookDiv.classList.add('new-book')
     bookBody.appendChild(newBookDiv)
-    
+   
     const bookAuthorDiv = document.createElement('div')
     bookAuthorDiv.classList.add('book-author')
     newBookDiv.appendChild(bookAuthorDiv)
-    bookAuthorDiv.innerText = `${userBook.author}`
+    bookAuthorDiv.innerText = `${bookObject.author}`
 
     const bookTitleDiv = document.createElement('div')
     bookTitleDiv.classList.add('book-title')
     newBookDiv.appendChild(bookTitleDiv)
-    bookTitleDiv.innerText = `${userBook.title}`
+    bookTitleDiv.innerText = `${bookObject.title}`
 
 
 const bookPagesDiv = document.createElement('div')
     bookPagesDiv.classList.add('book-pages')
     newBookDiv.appendChild(bookPagesDiv)
-    bookPagesDiv.innerText = `${userBook.pages}`
+    bookPagesDiv.innerText = `${bookObject.pages}`
     
-assignIndex(userBook, newBookDiv, bookAuthorDiv, bookTitleDiv, bookPagesDiv)        
-    // })
+assignIndex(bookObject, newBookDiv, bookAuthorDiv, bookTitleDiv, bookPagesDiv)        
+    
 }
 
 function assignIndex(bookObject, bookContainer, authorDiv, titleDiv, pagesDiv){
     let index = library.length;
     bookObject.id = index; 
     bookContainer.dataset.index = `${index}`;
-    authorDiv.dataset.index = `${index}`;
-    titleDiv.dataset.index = `${index}`;
-    pagesDiv.dataset.index = `${index}`;
+    let containerChildren = bookContainer.children;
+     for (let i = 0; i < containerChildren.length; i++) {
+    containerChildren[i].dataset.index =`${index}`
+        
+    }
 }
-/* function libraryEntry(){
-    createBook();
-    addBook();
-    createBookTile();
-} */
 
-const button = document.querySelector('[data-form-button]')
+
+const openButton = document.querySelector('[data-form-button]')
 const closeButton = document.querySelector('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-button.addEventListener('click', () =>{
-    const form = document.querySelector(button.dataset.formButton)
+openButton.addEventListener('click', () =>{
+    const form = document.querySelector(openButton.dataset.formButton)
     openForm(form)
 } )
 
@@ -117,7 +97,7 @@ submitButton.addEventListener('click', ()=>{
     const form = document.querySelector('.form-container.active')
 
     closeForm(form)
-    createBook()
+    createBook(addBook())
     clearFormValues()
     console.log(library)
 })
