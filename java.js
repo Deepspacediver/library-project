0df81ad4737bdffc07e1ba9276b2f2555df22204
deepspacediver <luka.koscik@gmail.com>
2022-07-11 19:59:24 +0200

    Added button indicating whether book has been read

--- java.js
+++ java.js
@@ -1,15 +1,15 @@
 let library = [];
 
-function Book(author, title, pages) {
+function Book(author, title, pages, isRead) {
     this.author = author
     this.title = title
     this.pages = pages
-    isRead = true;
+    this.isRead= isRead;
     this.originID=Date.now()
 }
 
 function addBook(){
-    const userBook = new Book(formAuthor.value, formTitle.value, formPages.value)
+    const userBook = new Book(formAuthor.value, formTitle.value, formPages.value, formRead.checked)
     library.push(userBook)
     return userBook
 }
@@ -44,16 +44,18 @@ function createBook(bookObject){
     removeButton.classList.add('remove-button')
     removeButton.innerText = 'Delete Book';
     newBookDiv.appendChild(removeButton)
-    
-    assignIndex(bookObject, newBookDiv)    
 
+    const readStatusButton = document.createElement('button')
+    readStatusButton.classList.add('read-status')
+    newBookDiv.appendChild(readStatusButton)
+    checkReadStatus(bookObject, readStatusButton)
+    assignIndex(bookObject, newBookDiv)  
 removeBook(removeButton)    
     
 }
 
 function assignIndex(bookObject, bookContainer){
     let bookID = bookObject.originID
-    console.log(bookID)
     bookContainer.dataset.index = `${bookID}`;
     let containerChildren = bookContainer.children;
      for (let i = 0; i < containerChildren.length; i++) {
@@ -61,6 +63,17 @@ function assignIndex(bookObject, bookContainer){
         
     }
 }
+function checkReadStatus(bookObject, button){
+    console.log(bookObject.isRead)
+    if (bookObject.isRead === true) {
+        button.classList.add('finished');
+    }return;
+}
+
+function changeReadstatus(){
+
+}
+
 
 let indexOfBook;
 function removeBook(removeButton){
@@ -116,18 +129,23 @@ const submitButton = document.querySelector('.submitForm')
 const formAuthor = document.getElementById('book-author');
 const formTitle = document.getElementById('book-title')
 const formPages = document.getElementById('book-pages')
+const formRead = document.getElementById('book-read');
 
 submitButton.addEventListener('click', ()=>{
     const form = document.querySelector('.form-container.active')
 
     closeForm(form)
+    
     createBook(addBook())
+
     clearFormValues()
+
 })
 
 function clearFormValues(){
     formAuthor.value ='';
     formTitle.value = '';
     formPages.value = '';
+    formRead.checked=''
 }
 
